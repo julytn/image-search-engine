d6fb44901faddc93003512e15f6e868dde991b57
Zoe Song <zoe.song@mail.utoronto.ca>
2017-02-21 10:40:01 -0500

    Handle lower and uppercase queries

--- clarifai_search.py
+++ clarifai_search.py
@@ -6,11 +6,8 @@ app = ClarifaiApp('8g-Q8QAGb9ShZ0wknjem0JxNd4oE1Iqi0d-6xPuF', '96BW52C5aZcNdkNNR
 
 def search(query, page=1, per_page=10):
 	try:
-		result = app.inputs.search_by_predicted_concepts(concept=query, page=page, per_page=per_page)
+		result = app.inputs.search_by_predicted_concepts(concept=query.lower(), page=page, per_page=per_page)
 	except ApiError as e:
 		return "Found 0 images with current search. Please try again."
 
 	return [img.url for img in result]
-
-
-print search('green')
