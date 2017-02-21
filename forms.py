8b896e84d6a7759b5205a645a75cb3e78f0ad9cd
Zoe Song <zoe.song@mail.utoronto.ca>
2017-02-21 14:24:09 -0500

    Fix indentation and add helpful message to search bar

--- forms.py
+++ forms.py
@@ -2,5 +2,5 @@ from flask_wtf import FlaskForm
 from wtforms import TextField, SubmitField, validators, ValidationError
 
 class SearchForm(FlaskForm):
-	query = TextField("Search for image", [validators.Required()])
+	query = TextField("Search for images in images.txt", [validators.Required()])
 	submit = SubmitField("Send")
